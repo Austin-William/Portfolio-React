8385b2d67b89826b1174d1e0c406b90e891ba1c8
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2026-09-20 08:51:17 +0200

    [REVERT] v1

--- v2/src/data/data.js
+++
@@ -1,9 +0,0 @@
-import projects_data from './json/projects.json';
-import experiences_data from './json/experiences.json';
-import services_data from './json/services.json';
-
-const projects = projects_data.projects_data;
-const experiences = experiences_data.experiences_data;
-const services = services_data.services_data;
-
-export { projects, experiences, services };
